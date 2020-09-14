cb8e2b44316f43a2119c6122d94cdb77d00f52e7
디팍 <3477488+deepakraous@users.noreply.github.com>
2020-09-14 09:02:33 -0700

    Updates to crypto.ex
    Settting up appropriate state, ref to State#

--- lib/crypto.ex
+++ lib/crypto.ex
@@ -1,6 +1,13 @@
 defmodule Crypto do
-    
-    # https://stackoverflow.com/questions/41735442
+
+    @doc ~S"""
+     Generates Random 32 bits
+     ## https://stackoverflow.com/questions/41735442
+     ##  
+
+      iex> [_] = ElixirBlocks.gen_random()
+           
+     """
     def gen_random() do
         length = 32
         :crypto.strong_rand_bytes(length) 
@@ -8,24 +15,36 @@ defmodule Crypto do
         |> binary_part(0, length)
     end
 
-    # Hash a given block
+    @doc ~S"""
+     Generates Random 32 bits
+     ## https://stackoverflow.com/questions/41735442
+     
+      iex> [_] = ElixirBlocks.hash("block")
+           
+     """
     def hash(%{}=block) do
         block
         |>Poison.encode16 # Convert to binary
         |>sha_256
     end
 
-    # Append the given block to an existing block
+    @doc ~S"""
+      Append the given block to an existing block
+      iex> [_] = ElixirBlocks.add_block("newBlock")         
+     """
     def add_block(%{}=block) do 
         %{block|hash: hash(block)}
     end
 
-    # Append the given block to an existing block
-    def sha_256(binary_data) do
-        binary_data
-        |>:keccakf1600.sha3_256
+    @doc ~S"""     
+       Sha256 Hash
+       iex> [_] = ElixirBlocks.sha_256(<<101,102>>)          
+     """
+    def sha_256(data) do
+
+        :crypto.hash(:sha256, <<101>>)
         |>Base.encode16
+        
     end
-  
     
 end
