9c629ed107e4472cde7276adbfbd3a1fa912a1c4
디팍 <3477488+deepakraous@users.noreply.github.com>
2020-09-21 19:55:22 -0700

    Update elixir_blocks.ex
    Removed code

--- lib/elixir_blocks.ex
+++ lib/elixir_blocks.ex
@@ -13,43 +13,8 @@ defmodule ElixirBlocks do
   import BloomFilter
   import Crypto
   import Trie
-  #import Recurser
+  import Recurser
  
-  #Genesis Info
-
-  # Source:MASS65*
-  # Sha256:232565FA051713BC8C67E58A38A34EBDE4B98AE2F168EFBA0A4BD16400E00CF3
-  @doc "Basic Block def"
-  def block do
-
-    block = %{
-      meta: "one_text",
-      address: "reciepient address of the block reward",
-      timestamp: "naive_datetime",
-      prev_hash: "hash_of_the_previous_block",
-      hash: "hash_of_the_current_block",
-      data: "[{k0,v0},{k1,v1},...,{Kn,Vn}]",
-      player: "hash(address(MASS65*))"
-    }
-
-  end
-
-  @doc "Genesis block def"
-  defp genesis_block do 
-
-    genesis = %{
-      meta: "MASS65*",
-      address: "abc",
-      timestamp: "0001 00:00:00",
-      prev_hash: "232565FA051713BC8C67E58A38A34EBDE4B98AE2F168EFBA0A4BD16400E00CF3",
-      hash: "hash(next(MASS65*)))",
-      data: "[{k0,v0},{k1,v1},...,{Kn,Vn}]",
-      player: "hash(address(MASS65*))"
-    
-    }    
-
-  end
-
   @doc "New Key Generation"
   defp new_key() do
     rand_string = Crypto.gen_random()
@@ -70,10 +35,10 @@ defmodule ElixirBlocks do
   def init(file_path) do
     # Blockchain 
     file_path = File.cwd!<>"/db"
-    block_chain = %{  }
+    block = %{  }
     {:ok, db} = CubDB.start_link(data_dir: file_path)
     key = new_key()
-    CubDB.put(db, key, genesis_block)
+    CubDB.put(db, key, block)
     [key, genesis_block]
   end
 
