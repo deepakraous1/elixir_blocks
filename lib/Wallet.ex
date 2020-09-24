769dcb679c56d51bcf794647c99b8dcd4959b8e9
디팍 <3477488+deepakraous@users.noreply.github.com>
2020-09-23 17:30:13 -0700

    Delete Wallet.ex

--- lib/Wallet.ex
+++
@@ -1,9 +0,0 @@
-#Will Contain wallet code
-
-# A wallet can contain max of 2 mapping
-
-# Map: [Address0:Value0, Address1:Value1]
-
-# Address is the first 160 bit SHA256 function of a Random value
-
-
