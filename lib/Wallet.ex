d0c43dcd8575e87fcea7268e11ad20820b060e3f
디팍 <3477488+deepakraous@users.noreply.github.com>
2020-09-23 17:27:47 -0700

    Create Wallet.ex
    Wallet address to contain

---
+++ lib/Wallet.ex
@@ -0,0 +1,9 @@
+#Will Contain wallet code
+
+# A wallet can contain max of 2 mapping
+
+# Map: [Address0:Value0, Address1:Value1]
+
+# Address is the first 160 bit SHA256 function of a Random value
+
+
