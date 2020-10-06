5050b7df4a34053ca00d5f297c5f896b6e86421b
디팍 <3477488+deepakraous@users.noreply.github.com>
2020-10-05 17:42:50 -0700

    Update blockchain.ex

--- lib/blockchain.ex
+++ lib/blockchain.ex
@@ -1,5 +1,6 @@
 import Block
 import Crypto
+
 defmodule Blockchain do
 
   def init do
@@ -9,18 +10,19 @@ defmodule Blockchain do
 
   @doc "Build a new block for given data and previous hash"
   def new(block_chain, data) do
-  
-  newblock = %{
-    meta: "RND*",
-    address_from:  "232565FA051713BC8C67E58",#"hash(address(MASS65*))"
-    address_to: "232565FA051713BC8C67E59",
-    timestamp: DateTime.utc_now(),
-    prev_hash: "232565FA051713BC8C67E58A38A34EBDE4B98AE2F168EFBA0A4BD16400E00CF3",
-    hash: "242565FA051713BC8C67E58A38A34EBDE4B98AE2F168EFBA0A4BD16400E00CF", #"hash(next(MASS65*)))",
-    data: "[{k1,v1},{k1,v1},...,{Kn,Vn}]"
-  } 
+    
+    newblock = %{
+      meta: "RND*",
+      address_from:  "232565FA051713BC8C67E58",#"hash(address(MASS65*))"
+      address_to: "232565FA051713BC8C67E59",
+      timestamp: DateTime.utc_now(),
+      prev_hash: "232565FA051713BC8C67E58A38A34EBDE4B98AE2F168EFBA0A4BD16400E00CF3",
+      hash: "242565FA051713BC8C67E58A38A34EBDE4B98AE2F168EFBA0A4BD16400E00CF", #"hash(next(MASS65*)))",
+      data: "[{k1,v1},{k1,v1},...,{Kn,Vn}]"
+    } 
 
-  [block_chain,newblock]
-end
+  
+
+  end
 
 end
